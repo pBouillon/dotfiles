6d0b92be092fb1c86cacc3b4b1be9c64b8a22773
Pierre Bouillon <22640284+pBouillon@users.noreply.github.com>
2024-07-28 11:06:42 +0200

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,77 +1,49 @@
-### %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-# Services
-
-## Starship
-
-### Launch on startup
+# Starship - Initialize
 Invoke-Expression (&starship init powershell)
-# Autocompletion
 
-## Enable autocomplete
+# fnm - Change environment on directory changed
+fnm env --use-on-cd | Out-String | Invoke-Expression
 
-### Shows navigable menu of all options when hitting Tab
+# Utility - Set Dev Location
+function Set-DevLocation {
+    param(
+        [string]$ProjectName
+    )
+
+    if (-not $ProjectName) {
+        Set-Location "~/Dev"
+        return
+    }
+
+    $projectPath = "~/Dev/Projects/$ProjectName"
+
+    if (Test-Path $projectPath) {
+        Set-Location $projectPath
+    } else {
+        Write-Host "No project named '$ProjectName' found." -ForegroundColor DarkYellow
+    }
+}
+
+Set-Alias dev Set-DevLocation
+
+# Utility - Touch alias
+function Create-File {
+    param(
+        [String]$FilePath
+    )
+    if (Test-Path -Path $FilePath) {
+        Write-Host "File already exists at '$FilePath'." -ForegroundColor DarkYellow
+    }
+    else{
+        New-Item $FilePath
+    }
+}
+
+Set-Alias touch Create-File
+
+# Autocompletion - Shows navigable menu of all options when hitting Tab
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
-### Autocompletion for arrow keys
+# Autocompletion - Show previous commands with same prefix
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
-
-### %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-# Aliases
-
-## Docker
-
-### Launch docker
-${function:startdocker} = { start "C:\Program Files\Docker\Docker\Docker Desktop.exe" }
-Set-Alias start-docker startdocker
-
-### `docker-compose` shortcut
-Set-Alias dc docker-compose
-
-### Restart the docker compose stack
-function DockerComposeRestart {
-    docker-compose down
-    docker-compose up -d
-}
-Set-Alias dcr DockerComposeRestart
-
-### Restart and rebuild the docker compose stack
-function DockerComposeRebuildRestart {
-    docker-compose down
-    docker-compose up -d --build
-}
-Set-Alias dcrr DockerComposeRebuildRestart
-
-## Productivity
-
-### Create an empty file
-
-function CreateFile([string]$Path) {
-    echo "" > $Path
-}
-Set-Alias touch CreateFile
-
-### Delete a folder
-
-function DeleteFolder([string]$Path) {
-    rm -Force -Recurse $Path
-}
-Set-Alias delete DeleteFolder
-
-### Sudo
-
-Set-Alias sudo gsudo
-
-## Scoop
-
-### Updater
-${function:UpdateScoop} = { scoop update; scoop update *; scoop cleanup *; scoop cache rm * }
-Set-Alias update-scoop UpdateScoop
-
-## Shortcuts
-
-### Open the dev directory
-${function:OpenDev} = { Set-Location ~/Documents/Dev }
-Set-Alias open-dev OpenDev
